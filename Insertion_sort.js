7540623c80c1ab0644c24f2789c333bf56fd6c62
Chris Chung <67829896+chris911024@users.noreply.github.com>
2022-11-24 19:26:55 +0800

    Add files via upload

---
+++ Insertion_sort.js
@@ -0,0 +1,12 @@
+function insertionsort(array) {
+  for (let i = 1; i < array.length; i++) {
+      for (let j = i; j > 0; j--) {
+          if(array[j] < array[j-1]) {
+              const temp = array[j];
+              array[j] = array[j-1];
+              array[j-1] = temp;
+          }
+      }
+  }
+  return array;
+}
